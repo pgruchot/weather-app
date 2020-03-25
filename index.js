8cb624aac8fc2eefc5693b16f920816cf9d12688
Patryk Gruchot <patrykg88@gmail.com>
2020-03-26 00:55:58 +0100

    API responding

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const requestPromise = require('request-promise');
 const keys = require('./config/keys')
-const router = express.Router();
 
 const app = express();
 
@@ -11,6 +10,28 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//Routes
+app.post('/checkweather', ( req, res ) => {
+
+    const { city } = req.body;
+    const apiKey = keys.weatherAPI.key;
+    console.log('kekw')
+    //Weather request configuration
+    const requestOptions = {
+        method: 'GET',
+        uri: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`,
+    };
+
+    requestPromise(requestOptions).then((weatherData) => {
+        //console.log(res)
+        res.json(weatherData);
+        }).catch((err) => {
+            console.log("Error while calling the API");
+            console.log(err)
+            res.json({err: 'An error occured. Please try again.'})
+        })  
+})
 
 //Port & listen
 const PORT = 3000;
